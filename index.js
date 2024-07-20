61dde11285f42f37646883974eecb749ef5538ee
devstack21 <lucdjob20@gmail.com>
2024-07-20 03:59:09 +0100

    gestion file error 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import path from 'path';
-import { fileURLToPath } from 'url'; // Import de fileURLToPath pour gérer __dirname
+import { fileURLToPath } from 'url';
 import fs from 'fs';
 import { sendMailer } from './sendMail.js';
 import { upload } from './middleware.file.js';
@@ -11,8 +11,8 @@ import deleteFileRecursively from './deleteFile.js';
 const app = express();
 dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url); // Définition de __filename
-const __dirname = path.dirname(__filename); // Définition de __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -20,17 +20,13 @@ app.post('/send/mail', upload.single('file'), async function (req, res) {
   const { subject, content, text, from, to, username } = req.body;
 
   if (!req.file) {
-    // Handle missing file upload gracefully
     return res.status(400).json({ message: 'No file uploaded' });
   }
 
-  const { originalname, filename, path: filePath } = req.file; // Extract file information
-
-  // Use path.join to create the file path
+  const { originalname, filename, path: filePath } = req.file;
   const absoluteFilePath = path.join(__dirname, filePath);
 
   try {
-    // Ensure the file exists at the given path
     if (!fs.existsSync(absoluteFilePath)) {
       return res.status(404).json({ message: 'File not found' });
     }
@@ -41,8 +37,7 @@ app.post('/send/mail', upload.single('file'), async function (req, res) {
     console.error('Error:', error);
     res.status(500).json({ message: error.message });
   } finally {
-    // Delete the uploaded file after sending
-    await deleteFileRecursively(absoluteFilePath); 
+    await deleteFileRecursively(absoluteFilePath);
   }
 });
 
