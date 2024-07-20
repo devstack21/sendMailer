97e6b9484a4a1a45418fa5f347be3a0339bbb31a
devstack21 <lucdjob20@gmail.com>
2024-07-20 03:44:03 +0100

    gestion file error

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import path from 'path';
+import fs from 'fs';
 import { sendMailer } from './sendMail.js';
 import { upload } from './middleware.file.js';
 import deleteFileRecursively from './deleteFile.js';
@@ -20,21 +21,29 @@ app.post('/send/mail', upload.single('file'), async function (req, res) {
   }
 
   const { originalname, filename, path: filePath } = req.file; // Extract file information
+  const absoluteFilePath = path.join(__dirname, filePath); // Ensure the path is absolute
 
   try {
-    await sendMailer(originalname, filePath, subject, content, text, from, to, username);
+    // Ensure the file exists at the given path
+    if (!fs.existsSync(absoluteFilePath)) {
+      return res.status(404).json({ message: 'File not found' });
+    }
+
+    await sendMailer(originalname, absoluteFilePath, subject, content, text, from, to, username);
     res.status(200).json({ message: 'Email sent successfully' });
   } catch (error) {
     console.error('Error:', error);
     res.status(500).json({ message: error.message });
   } finally {
-    await deleteFileRecursively(filePath); // Delete the uploaded file after sending
+    // Delete the uploaded file after sending
+    await deleteFileRecursively(absoluteFilePath); 
   }
-})
-app.get('/' , function(req , res){
-    res.status(200).json({message : 'main Endpoints '})
-})
+});
+
+app.get('/', function(req, res) {
+  res.status(200).json({ message: 'Main Endpoints' });
+});
 
 app.listen(3000, function () {
-  console.log('Server started on the', 3000);
+  console.log('Server started on port', 3000);
 });
